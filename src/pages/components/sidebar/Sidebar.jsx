22235af69f258452077a92e88f522f4013ad32a1
Moinul-Islam-Raj <moinulislamraj0@gmail.com>
2024-03-18 18:54:41 +0600

    height fix

--- src/pages/components/sidebar/Sidebar.jsx
+++ src/pages/components/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import Conversations from './Conversations'
  
  const Sidebar = () => {
    return (
-     <div className='bg-slate-600 md:w-1/3 w-full p-4 min-w-48 relative md:h-full h-2/6'>
+     <div className='bg-slate-600 md:w-1/3 w-full p-4 min-w-48 relative md:h-full h-2/6 min-h-[300px]'>
         <Search />
         <div className='w-full bg-slate-500 h-[2px] mb-2'></div>
         <Conversations/>

dd7a951038a18b2afadd7ffecd73ac82183cba75
Moinul-Islam-Raj <moinulislamraj0@gmail.com>
2024-03-20 12:17:38 +0600

    auto scroll

--- src/pages/components/main/Messages.jsx
+++ src/pages/components/main/Messages.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
 import useMessages from '../../../hooks/useMessages'
 import {useListenForMessages} from '../../../hooks/useListenForMessages'
 
 const Messages = () => {
   const [loading, messages] = useMessages();
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      ref.current?.lastElementChild?.scrollIntoView()
+    }, 150);
+  }, [messages])
+
   useListenForMessages();
   return (
-    <div className='h-4/5 overflow-auto p-4 w-full text-white'>
+    <div ref={ref} className='h-4/5 overflow-auto p-4 w-full text-white'>
       {messages.map((m, i) => (
         <Message message={m} key={i}/>
       ))}
